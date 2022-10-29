ab17996aee9e0dbd6deb99d57d085e4506917c33
WARLOCK77 <rometotalwar1174@gmail.com>
2022-10-29 16:28:56 +0300

    comRepair1

--- src/main/kotlin/ua/levelup/homework23/Homework23.kt
+++ src/main/kotlin/ua/levelup/homework23/Homework23.kt
@@ -1,32 +1,34 @@
 package ua.levelup.homework23
 
+import kotlin.math.pow
+
 fun main (args: Array<String>) {
-    println(pow(-3, 2.0))
+    println(pow(3, 2))
     println(filter("kotlin", 'k','b','j','i'))
 
 }
 
-fun pow(power: Int, number: Double): Double {
-    var result =0.0
-    if (power<0){
-        var res: Double=number
-        for (i in power..-1 ){
-            res*=number
-        }
-        result=1/res
-    }
-    if (power>1){
-        var res: Double=number
-        for (i in 2..power) {
-            res *= number
-        }
-        result=res
-    }
-    when (power){
-        0-> return 1.0
-        1-> return number
-    }
-    return result
+fun pow(power: Int, number: Int): Double {
+//    var result =0.0
+//    if (power<0){
+//        var res: Double=number
+//        for (i in power..-1 ){
+//            res*=number
+//        }
+//        result=1/res
+//    }
+//    if (power>1){
+//        var res: Double=number
+//        for (i in 2..power) {
+//            res *= number
+//        }
+//        result=res
+//    }
+//    when (power){
+//        0-> return 1.0
+//        1-> return number
+//    }
+    return number.toDouble().pow(power)
 }
 
 fun filter(string:String, vararg symbols: Char):String{
